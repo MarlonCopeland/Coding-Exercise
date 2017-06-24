7b8deeec9dd3e082d1af7725192c32a3137aae00
Marlon Copeland <mcopeland26@msn.com>
2017-06-24 17:46:42 -0400

    First commit

---
+++ CodingExercise/CodingExerciseClassicDotNet/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExerciseClassicDotNet
+{
+    class Animal
+    {
+        //properties of this class:
+        private int weight;
+
+        /// <summary>
+        /// The Accessor for the private weight property. Differentiated
+        /// by the Capital W in the name of the variable. The GET accessor will return
+        /// the private value for lowecase weight. The SET accessor will set the private
+        /// propert to the value assigned thought the public variable. This is an example of
+        /// ENCAPSULATION.
+        /// </summary>
+        public int Weight {
+            get {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+
+        private string name { get; set; }
+
+        private string height;
+
+        private int numberOfLimbs;
+
+
+        public bool IsAlive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodingExercise/CodingExerciseClassicDotNet/LivingThing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExerciseClassicDotNet
+{
+    /// <summary>
+    /// An Interface is a blueprint for an object. when an object inherits from this interface, it must use the methods it holds!
+    /// </summary>
+    interface LivingThing
+    {
+        /// <summary>
+        /// this method must be implemented by any class that implements this interface!
+        /// </summary>
+        /// <returns></returns>
+        bool IsAlive();
+
+    }
+}
